c1d9f6749143f71112cc9a6b9b945c4fb2dfd4b1
Matheus de Campos <matheus.campos@edu.unifil.br>
2022-04-29 09:37:16 -0300

    feat: add configurations to tables and properties to database.

---
+++ src/Infra/Mappings/TransactionMapping.cs
@@ -0,0 +1,47 @@
+﻿using Domain;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Repository.Mappings
+{
+    public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("tbl_transaction");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnType("integer")
+                .HasColumnName("id");
+
+            builder.Property(x => x.TransactionTypeId)
+                .HasColumnType("integer")
+                .HasColumnName("fk_transaction_type_id");
+
+            builder.Property(x => x.Date)
+                .HasColumnType("varchar(10)")
+                .HasColumnName("date");
+
+            builder.Property(x => x.Amount)
+                .HasColumnType("decimal(18,2)")
+                .HasColumnName("amount");
+
+            builder.Property(x => x.CPF)
+                .HasColumnType("varchar(11)")
+                .HasColumnName("cpf");
+
+            builder.Property(x => x.CardNumber)
+                .HasColumnType("varchar(16)")
+                .HasColumnName("card_number");
+
+            builder.Property(x => x.Hour)
+                .HasColumnType("time(7)")
+                .HasColumnName("hour");
+
+            builder.HasOne(x => x.TransactionType)
+                .WithMany(x => x.Transactions)
+                .HasForeignKey(x => x.TransactionTypeId);
+        }
+    }
+}
---
+++ src/Infra/Mappings/TransactionTypeMapping.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Mappings
+{
+    public class TransactionTypeMapping : IEntityTypeConfiguration<TransactionType>
+    {
+        public void Configure(EntityTypeBuilder<TransactionType> builder)
+        {
+            builder.ToTable("tbl_transaction_type");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnType("integer")
+                .HasColumnName("id");
+
+            builder.Property(x => x.Description)
+                .HasColumnType("varchar(20)")
+                .HasColumnName("description");
+
+            builder.Property(x => x.Nature)
+                .HasColumnType("varchar(20)")
+                .HasColumnName("nature");
+
+            builder.Property(x => x.Signal)
+                .HasColumnType("varchar(1)")
+                .HasColumnName("signal");
+
+            builder.HasData(new TransactionType(1,"Débito", "Entrada", "+"));
+            builder.HasData(new TransactionType(2,"Boleto", "Saída", "-"));
+            builder.HasData(new TransactionType(3,"Financiamento", "Saída", "-"));
+            builder.HasData(new TransactionType(4,"Crédito", "Entrada", "+"));
+            builder.HasData(new TransactionType(5,"Recebimento Entrada", "Entrada", "+"));
+            builder.HasData(new TransactionType(6,"Vendas", "Entrada", "+"));
+            builder.HasData(new TransactionType(7,"Recebimento TED", "Entrada", "+"));
+            builder.HasData(new TransactionType(8,"Recebimento DOC", "Entrada", "+"));
+            builder.HasData(new TransactionType(9,"Aluguél", "Saída", "-"));
+        }
+    }
+}
