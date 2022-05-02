45bcd149b3c5e0b0baa361b24bf1a66dde13ec46
Matheus de Campos <matheus.campos@edu.unifil.br>
2022-05-02 09:21:39 -0300

    feat: implenting reader.

---
+++ src/Api/Utils/Helper.cs
@@ -0,0 +1,26 @@
+﻿namespace Api.Utils
+{
+    public static class Helper
+    {
+        public static async Task<string> ReadFormFileAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return await Task.FromResult((string)null);
+            }
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        public static bool IsTxtExtension(IFormFile file)
+        {
+            FileInfo fileInfo = new FileInfo(file.FileName);
+            if (fileInfo.Extension == ".txt")
+                return true;
+            return false;
+        }
+    }
+}
