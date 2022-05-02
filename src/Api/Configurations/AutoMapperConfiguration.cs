3b56d3c61f034cfe7121330d9ce0941ba1394bbc
Matheus de Campos <matheus.campos@edu.unifil.br>
2022-05-02 09:20:05 -0300

    feat: add automapper configuration to dtos and generic class response.

---
+++ src/Api/Configurations/AutoMapperConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Api.ViewModels;
+using AutoMapper;
+using Domain;
+using Domain.Dtos;
+using Domain.Entities;
+using System.Text.Json.Serialization;
+
+namespace Api.Configurations
+{
+    public class AutoMapperConfiguration: Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<Transaction, CreateTransactionDto>().ReverseMap();
+
+            CreateMap<Transaction, GetTransactionDto>();
+            CreateMap<ApiResponse<IEnumerable<Transaction>>, ApiResponse<IEnumerable<GetTransactionDto>>>().ReverseMap();
+            CreateMap<TransactionType, GetTransactionTypeDto>()
+                .ReverseMap()
+                .ForPath(x => x.Transactions, opt => opt.Ignore());
+        }
+    }
+}
---
+++ src/Domain/Entities/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class ApiResponse<T>
+    {
+        public T Data { get; private set; }
+        public decimal TotalAmount { get; private set; }
+
+        public static ApiResponse<T> Success(T data, decimal totalAmount)
+        {
+            return new ApiResponse<T> { Data = data, TotalAmount = totalAmount };
+        }
+
+    }
+}
